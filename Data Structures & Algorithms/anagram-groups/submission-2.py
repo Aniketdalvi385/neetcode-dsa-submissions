13a84c1f1d91b8408150021f7ef11b0da287a4de
Aniket Dalvi <113906569+Aniketdalvi385@users.noreply.github.com>
2026-06-04 10:47:58 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Brute Force solution with really high complexity time: O(n^2*m)
+        # def checkAnagram(s: str, t: str) -> bool:
+        #     if len(s) != len(t): return False
+        #     seen = dict()
+        #     for ch in s:
+        #         seen[ch] = seen.get(ch, 0) + 1
+
+        #     for ch in t:
+        #         if ch not in seen or seen[ch] == 0:
+        #             return False
+        #         seen[ch] -= 1
+            
+        #     return True
+
+        # res = []
+        # for i in strs:
+        #     isValid = False
+        #     for j in res:
+        #         isValid = checkAnagram(i , j[0])
+        #         if isValid:
+        #             j.append(i)
+        #             break
+        #     if not isValid:
+        #         res.append([i])
+
+        # return res
+
+        map = dict()
+        for s in strs:
+            key = "".join(sorted(s))
+            if key in map:
+                map[key].append(s)
+            else:
+                map[key] = [s]
+        return list(map.values())
+    
