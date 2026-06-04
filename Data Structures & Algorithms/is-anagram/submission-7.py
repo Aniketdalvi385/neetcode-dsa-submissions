3e05aa491f764c7b1c3f37028930db714ead780c
Aniket Dalvi <113906569+Aniketdalvi385@users.noreply.github.com>
2026-06-04 11:33:52 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Brute Force solution 
+        if len(s) != len(t): return False
+        map = dict()
+        for ch in s:
+            map[ch] = map.get(ch, 0) + 1
+
+        for ch1 in t:
+            if ch1 not in map or map[ch1] == 0:
+                return False
+            map[ch1] -= 1
+        
+        return True
+
+        # Trying with canonical key approach not optimal but fun complexity: O(n log n)
+        # if len(s) != len(t): return False
+        # skey = sorted(s)
+        # tkey = sorted(t)
+        # for i, ch in enumerate(skey):
+        #     if ch != tkey[i]:
+        #         return False
+        # return True
