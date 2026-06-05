23a3ca840a5f131dda3d45fa56d78501cdf94379
Aniket Dalvi <113906569+Aniketdalvi385@users.noreply.github.com>
2026-06-05 11:40:10 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        map = {}
+        res = []
+        for i in nums:
+            map[i] = map.get(i, 0) + 1
+            if len(res) < k and i not in res:
+                res.append(i)
+            elif i not in res:
+                for ind, j in enumerate(res):
+                    if map[j] < map[i]:
+                        res[ind] = i
+                        break
+        return res
