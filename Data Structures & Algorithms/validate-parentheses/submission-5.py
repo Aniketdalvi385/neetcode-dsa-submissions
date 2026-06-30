a08971b0fee1bc95907c3710bfee0612a76815d2
Aniket Dalvi <113906569+Aniketdalvi385@users.noreply.github.com>
2026-06-30 17:36:17 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,52 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Brute force
+        # if len(s)%2 != 0:
+        #     return False
+        # curr = deque()
+        # for i in s:
+        #     print(curr)
+        #     if i == '[' or i == '(' or i == '{':
+        #         curr.append(i)
+        #     else:
+        #         if i == ']' and curr:
+        #             if curr[-1] != '[':
+        #                 return False
+        #             else:
+        #                 curr.pop()
+        #         elif i == ')' and curr:
+        #             if curr[-1] != '(':
+        #                 return False
+        #             else:
+        #                 curr.pop()
+        #         elif i == '}' and curr:
+        #             if curr[-1] != '{':
+        #                 return False
+        #             else:
+        #                 curr.pop()
+        #         else:
+        #             return False
+        # if curr:
+        #     return False
+        
+        # return True
+
+        if len(s)%2 != 0:
+            return False
+
+        hashmap = {')': '(', '}':'{', ']':'['}
+        stack = []
+        for ch in s:
+            if ch in hashmap and stack:
+                if stack[-1] != hashmap[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        if stack:
+            return False
+
+        return True
+
+
