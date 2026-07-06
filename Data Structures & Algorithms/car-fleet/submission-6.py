8ec5f3c76ef16c1d5497e5f31645e07222fe7416
Aniket Dalvi <113906569+Aniketdalvi385@users.noreply.github.com>
2026-07-06 15:41:18 +0530

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # Optimal Solution Time complexity: O(n log n) Space Complexity: O(n)
+        # cars = sorted(zip(position, speed), reverse=True)
+        # stack = []
+        # for car in cars:
+        #     time = (target - car[0])/car[1]
+        #     if not stack or stack[-1] < time:
+        #         stack.append(time)
+
+        # return len(stack)
+
+        # Second Approach with simple hashmap to sorted
+        hashmap = {}
+        for i in range(len(position)):
+            hashmap[position[i]] = speed[i]
+
+        position = sorted(position, reverse=True)
+        print(position)
+        prev = 0
+        count = 0
+        for car in position:
+            time = (target - car)/hashmap[car]
+            print(time)
+            if prev < time:
+                count += 1
+                prev = time
+        
+        return count
