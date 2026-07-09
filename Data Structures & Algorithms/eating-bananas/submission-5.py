0ff965eb6e8b7bfdaccb179d404254dfe0474d02
Aniket Dalvi <113906569+Aniketdalvi385@users.noreply.github.com>
2026-07-09 09:36:48 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Optimal Soultion Time Complexity: O(n log m) Space Complexity: O(1)
+        # maxB = 0
+        # for i in piles:
+        #     maxB = max(maxB, i)
+        
+        # lo, hi = 1, maxB
+
+        # lastValid = hi
+        
+        # while lo <= hi:
+        #     mid = lo + (hi - lo)//2
+        #     count = 0
+        #     for i in piles:
+        #         count += (i+mid-1)//mid
+        #     if count <= h:
+        #         lastValid = mid
+        #         hi = mid - 1
+        #     else: 
+        #         lo = mid + 1
+
+        # return lastValid
+
+        # Cleaner Version
+
+        lo, hi = 1, max(piles)
+        lastValid = hi
+
+        while lo <= hi:
+            mid = lo + (hi - lo)//2
+            hours = sum((i+mid-1)//mid for i in piles)
+            if hours <= h:
+                lastValid = mid
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        
+        return lastValid
